c4be1feb0f5f951608da91ff0c23ff6788c46f8f
shengcn <scn_2001_ren@163.com>
2022-03-14 00:17:21 +0800

    fix bugs

--- knapsack.py
+++ knapsack.py
@@ -38,27 +38,29 @@ class Knapsack:
 
     def __search_recursive(self, max_cost, id):
 
-        item = self.items[id]
-
         if (max_cost, id) in self.search_table:
             return self.search_table[(max_cost, id)]
 
-        if max_cost < item.cost:
+        if id == 0:
             self.search_table[(max_cost, id)] = (0, None)
             return self.search_table[(max_cost, id)]
-        elif id == 1:
-            self.search_table[(max_cost, id)] = (item.value, (None, item))
+
+        item = self.items[id]
+
+        if max_cost < item.cost:
+            self.search_table[(max_cost, id)] = self.__search_recursive(max_cost, id - 1)
             return self.search_table[(max_cost, id)]
 
-        value_1, last_item_1 = self.__search_recursive(max_cost - item.cost, id - 1)
-        value_2, last_item_2 = self.__search_recursive(max_cost, id - 1)
-
-        if value_1 + item.value > value_2:
-            self.search_table[(max_cost, id)] = (value_1 + item.value, (last_item_1, item))
         else:
-            self.search_table[(max_cost, id)] = (value_2, last_item_2)
+            value_1, last_item_1 = self.__search_recursive(max_cost - item.cost, id - 1)
+            value_2, last_item_2 = self.__search_recursive(max_cost, id - 1)
 
-        return self.search_table[(max_cost, id)]
+            if value_1 + item.value > value_2:
+                self.search_table[(max_cost, id)] = (value_1 + item.value, (last_item_1, item))
+            else:
+                self.search_table[(max_cost, id)] = (value_2, last_item_2)
+
+            return self.search_table[(max_cost, id)]
 
 
     def optimized_solution_recursive(self, max_cost:int):
@@ -77,51 +79,57 @@ class Knapsack:
         return max_value, selected_items
 
 
-    def __stack_search(self, stack:list):
+    def __search_stack(self, stack:list):
 
         max_cost = stack[-1].max_cost
         id = stack[-1].id
-
-        item = self.items[id]
 
         if (max_cost, id) in self.search_table:
             stack.pop()
             return self.search_table[(max_cost, id)]
 
-        if max_cost < item.cost:
+        if id == 0:
             self.search_table[(max_cost, id)] = (0, None)
             stack.pop()
             return self.search_table[(max_cost, id)]
-        elif id == 1:
-            self.search_table[(max_cost, id)] = (item.value, (None, item))
-            stack.pop()
-            return self.search_table[(max_cost, id)]
+
+        item = self.items[id]
         
-        elif len(stack[-1].results_of_subsearch) == 0:
-
-            stack.append(StackItem(max_cost - item.cost, id - 1))
-            
-            return None
-
-        elif len(stack[-1].results_of_subsearch) == 1:
-
-            stack.append(StackItem(max_cost, id - 1))
-
-            return None
-
-        elif len(stack[-1].results_of_subsearch) == 2:
-
-            value_1, last_item_1 = stack[-1].results_of_subsearch[0]
-            value_2, last_item_2 = stack[-1].results_of_subsearch[1]
-
-            if value_1 + item.value > value_2:
-                self.search_table[(max_cost, id)] = (value_1 + item.value, (last_item_1, item))
+        if max_cost < item.cost:
+            if len(stack[-1].results_of_subsearch) == 0:
+                stack.append(StackItem(max_cost, id - 1))
+                return None
             else:
-                self.search_table[(max_cost, id)] = (value_2, last_item_2)
+                self.search_table[(max_cost, id)] = stack[-1].results_of_subsearch[0]
 
-            stack.pop()
-            
-            return self.search_table[(max_cost, id)]
+                stack.pop()
+
+                return self.search_table[(max_cost, id)]
+        
+        else:
+            if len(stack[-1].results_of_subsearch) == 0:
+                stack.append(StackItem(max_cost - item.cost, id - 1))
+                
+                return None
+
+            elif len(stack[-1].results_of_subsearch) == 1:
+                stack.append(StackItem(max_cost, id - 1))
+
+                return None
+
+            elif len(stack[-1].results_of_subsearch) == 2:
+
+                value_1, last_item_1 = stack[-1].results_of_subsearch[0]
+                value_2, last_item_2 = stack[-1].results_of_subsearch[1]
+
+                if value_1 + item.value > value_2:
+                    self.search_table[(max_cost, id)] = (value_1 + item.value, (last_item_1, item))
+                else:
+                    self.search_table[(max_cost, id)] = (value_2, last_item_2)
+
+                stack.pop()
+                
+                return self.search_table[(max_cost, id)]
 
 
     def optimized_solution_of_stack_mode(self, max_cost:int):
@@ -132,7 +140,7 @@ class Knapsack:
         result = None
 
         while len(the_stack) > 0:
-            result = self.__stack_search(the_stack)
+            result = self.__search_stack(the_stack)
             if not (result is None) and len(the_stack) > 0:
                 the_stack[-1].results_of_subsearch.append(result)
             else:
@@ -152,6 +160,8 @@ class Knapsack:
 
 def test_case(n_items, value_range, cost_range, max_cost):
 
+    print('------------------------------------------------------------')
+
     items = dict()
 
     for i in range(n_items):
@@ -159,6 +169,11 @@ def test_case(n_items, value_range, cost_range, max_cost):
             value = random.randint(value_range[0], value_range[1]),
             cost = random.randint(cost_range[0], cost_range[1]))
         items[item.id] = item
+
+    for id in sorted(items.keys()):
+        item = items[id]
+        print('({}, {}, {})'.format(id, item.value, item.cost), end=' ')
+    print()
 
     model = Knapsack(items)
     
@@ -235,12 +250,16 @@ if __name__ == "__main__":
 
     random.seed(10)
 
+    for i in range(10):
+        test_case(8, (1, 10), (1, 10), 2)
+        KnapsackItem.STATIC_ID = 0
+
     for i in range(100):
-        cross_test_case(200, (1, 20), (1, 20), 100)
+        cross_test_case(100, (1, 20), (1, 20), 200)
         KnapsackItem.STATIC_ID = 0
 
     for i in range(10):
-        test_case(2000, (1, 50), (1, 50), 500)
+        test_case(1200, (1, 50), (1, 50), 50)
         KnapsackItem.STATIC_ID = 0
 
     
